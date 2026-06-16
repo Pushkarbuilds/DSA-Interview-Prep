a3ef681278c26f5a326e0f14dbbcdaa4f6fcf4c4
Pushkar Sharma <pswork779@gmail.com>
2026-06-16 19:12:39 +0530

    First DSA problem

---
+++ Arrays/TwoSum.py
@@ -0,0 +1,16 @@
+def twoSum(nums, target):
+    hashmap = {}
+
+    for i, num in enumerate(nums):
+        diff = target - num
+
+        if diff in hashmap:
+            return [hashmap[diff], i]
+
+        hashmap[num] = i
+
+
+nums = [2, 7, 11, 15]
+target = 9
+
+print(twoSum(nums, target))
